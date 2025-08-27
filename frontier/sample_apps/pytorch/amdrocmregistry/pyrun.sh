a362d6fff56b49c9ea34ce6b90fc9dc7240454d9
Subil Abraham <abrahams@ornl.gov>
2025-08-27 16:32:29 -0400

    removing unneeded miniforge init

--- frontier/sample_apps/pytorch/amdrocmregistry/pyrun.sh
+++ frontier/sample_apps/pytorch/amdrocmregistry/pyrun.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-if [[ -f /opt/miniforge/bin/activate ]]; then
-    source /opt/miniforge/bin/activate
-fi
 python3 -W ignore -u ./multinode_olcf_mnist_princeton_nompi4py.py --master_addr=$MASTER_ADDR --master_port=3442

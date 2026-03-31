ec46213b48316eb1ff87f0c7a9b999b37dbff4a6
Subil <abrahams@ornl.gov>
2026-03-31 17:09:31 -0400

    Adding debugging example
    This is meant to accompany the Debugging section in the OLCF containers
    on Frontier documentation.

---
+++ frontier/containers_on_frontier_docs/debugging/launchapp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $SLURM_PROCID -eq 0 ]]; then
+    # this will launch a gdbserver with the MPI rank 0
+    #gdbserver 0.0.0.0:2345 /opt/mpi_bcast
+    gdbserver 0.0.0.0:2345  /opt/lammps/bin/lmp -k on g 1 -sf kk -pk kokkos gpu/aware on -in ij.in
+else
+    # this launches all the other MPI ranks normally
+    #/opt/mpi_bcast
+    /opt/lammps/bin/lmp -k on g 1 -sf kk -pk kokkos gpu/aware on -in ij.in
+fi
